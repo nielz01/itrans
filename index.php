78fdc7305ef03fddd27f45114912c76ce67e94a3
nielz01 <d3d421@gmail.com>
2023-06-28 20:05:46 +0700

    coba

--- index.php
+++ index.php
@@ -10,26 +10,25 @@ check_login();
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-		crossorigin="anonymous"></script>
-
-
+		integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+		</script>
 </head>
 
 <body>
 	<?php include "navbar.php"; ?>
-	<div class="container"> 
-	<table>
-	<tr>
-		<td></td>
-	</tr>
-		<div class="row" >
-			<div class="col">
-			<?php include "slide_show.php"; ?>
+	<div class="container">
+		<table>
+			<tr>
+				<td></td>
+			</tr>
+			<div class="row">
+				<div class="col">
+					<?php include "slide_show.php"; ?>
+				</div>
 			</div>
-		</div>
 	</div>
-	</nav>
+
+
 </body>
 
 </html>
